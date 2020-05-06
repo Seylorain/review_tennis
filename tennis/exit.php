e8c8577fc971a2517a509a8af31ff3668a4be20b
Seyloreyn <36475764+Seyloreyn@users.noreply.github.com>
2020-05-06 19:55:43 +0300

    Update exit.php

--- tennis/exit.php
+++ tennis/exit.php
@@ -3,11 +3,11 @@
 CWE  				98: Improper Control of Filename for Include/Require Statement in PHP Program ('PHP Remote File Inclusion')
 
 Описание: 			Ошибка связана с прямым использованием функции include_once('db.php'). 
-					Можно сформировать hhtp запрос таким образом, что удастся внедрить php модуль с чужого хостинга.
+				Можно сформировать hhtp запрос таким образом, что удастся внедрить php модуль с чужого хостинга.
 
-Решение проблемы: 	1)	Нумеровать модули следующем образом: “module1.php”, ”module2.php” …”module<n>.php”
-						Преобразовать $module в числовой формат (settype($module,”integer”)) 
-					2) 	Использовать конструкцию switch-case.
+Решение проблемы: 		1)Нумеровать модули следующем образом: “module1.php”, ”module2.php” …”module<n>.php”
+				Преобразовать $module в числовой формат (settype($module,”integer”)) 
+				2) Использовать конструкцию switch-case.
 					switch ($case) // $case - имя переменной передаваемой в параметре к скрипту
 					{
 						case news:
@@ -21,7 +21,8 @@ CWE  				98: Improper Control of Filename for Include/Require Statement in PHP P
 						default:
 						include("index.php"); // если в переменной $case не будет передано значение, которое учтено выше, то открывается главная страница
 						break;
-					}			 
+					}	
+					
 Источник: 			http://www.realcoding.net/articles/php-include-uyazvimost-ot-teorii-k-praktike.html
 */
 include_once('db.php');
